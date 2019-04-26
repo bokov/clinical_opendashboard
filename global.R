a62a0715cf2103b9e6059bb9174e6f0f806c676f
Alex Bokov <bokov@uthscsa.edu>
2019-04-26 14:18:37 -0500

    global.R: For some reason the readr dependency I thought I introduced last time didn't take. Added it again. Also, sourcing uitext.R and creating selBasicChoices for populating variable selection menu

--- global.R
+++ global.R
@@ -1,3 +1,11 @@
+library(readr);
 # selected prefixes and concept codes 
 demogcodes <- read_csv('demogcodes.csv');
+# demogcodes should include the following columns: 'PREFIX','Category','CCD'
+# PREFIX and CCD are the same as used by ChinoType. Category is a human-readable
+# version of PREFIX.
+selBasicChoices <- with(unique(demogcodes[,c('PREFIX','Category')])
+                        ,setNames(PREFIX,Category));
+# UI text
+source('uitext.R');
 c()
