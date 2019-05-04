48d1dc467b6930114d6e4bef6b196d5c3c0281ef
Alex Bokov <bokov@uthscsa.edu>
2019-05-03 21:15:29 -0500

    ui.R: Sliders get their default values from the slidevals list. Added a button to reset sliders to their default values. Rendering of debug button now triggered by existence of .debug file in app directory

--- ui.R
+++ ui.R
@@ -29,20 +29,23 @@ shinyUI(fluidPage(
                                                       ,icon('angle-down'))
                                           ,sliderInput('slN','Count cutoff'
                                                        ,min=50,max=2000
-                                                       ,value=300,step = 1)
+                                                       ,value=slidevals$N
+                                                       ,step = 1)
                                           ,sliderInput('slOR'
                                                        ,'Odds ratio cutoff'
-                                                       ,min=1.1,max=4,value=1.5
+                                                       ,min=1.1,max=4
+                                                       ,value=slidevals$OR
                                                        ,step=0.01)
                                           ,sliderInput('slChi'
                                                        ,'Chi-square cutoff'
                                                        ,min=0,max=1000
-                                                       ,value=200)
-                                          
+                                                       ,value=slidevals$Chi)
+                                          ,actionButton('breset'
+                                                        ,'Reset Sliders')
                                           )
                          )
              ,actionButton('bupdate','Update')
-             #,actionButton('bdebug','Debug')
+             ,if(file.exists('.debug')) actionButton('bdebug','Debug') else c()
              )
       ,column(10,textOutput('maintext'),br()
               ,plotlyOutput('plotmain',width = '79vmin',height = '70vmin'))
