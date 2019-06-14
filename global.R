5d8ffee17fd3bffefb23665237386e02b15eea2b
Alex Bokov <bokov@uthscsa.edu>
2019-06-14 13:49:50 -0500

    global.R: added prefixbars vector (all the prefixes that are NOT to be used for scatterplots) and a prefilter list (arguments for the initial, non-interactive invokation of chifilter() upon loading of the data in server.R)

--- global.R
+++ global.R
@@ -6,11 +6,15 @@ demogcodes <- if(file.exists('demogcodes.csv')){
   read_csv('demogcodes.csv')} else {read_csv('demogcodes_demo.csv')};
 # prefixes whose variables should be rendered as scatter-plots
 prefixpoints <- subset(demogcodes,is.na(CCD))$PREFIX;
+# someday if the TOTAL row ceases to be invariant in ChinoType, this will break
+prefixbars <- setdiff(demogcodes$PREFIX,c('TOTAL',prefixpoints));
 # demogcodes should include the following columns: 'PREFIX','Category','CCD'
 # PREFIX and CCD are the same as used by ChinoType. Category is a human-readable
 # version of PREFIX.
 selBasicChoices <- with(unique(demogcodes[,c('PREFIX','Category')])
                         ,setNames(PREFIX,Category));
+# for prefiltering input
+prefilter <- list(N=0,OR=1,Chi=1);
 # slider defaults
 slidevals <- list(N=300,OR=1.5
                   #,Chi=200
