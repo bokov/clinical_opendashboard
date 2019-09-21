40a2cfec4c6186b30f416e3a942c25ba5aa64e77
Alex Bokov <bokov@uthscsa.edu>
2019-09-20 22:22:21 -0500

    infiles/demo;config;Various changes to prepare for simulated demo data

--- infiles/demo/project_custom.R
+++ infiles/demo/project_custom.R
@@ -1,23 +1,24 @@
 # The minimum fraction of the reference group that must have a code for that
 # code to be included
-minfrac <- 0.01;
+minfrac <- 0.05;
 
 # The minimun count of positives from the reference group for a code to be 
 # included
-mincount <- 100;
+mincount <- 500;
 
 # Any other custom selection criteria for codes
 customfilter <- TRUE;
 
 # Which files to load in this directory
-dfiles <- c('urology_lowincome.csv','urology_hispanic.csv');
+dfiles <- c('rand85.csv','rand89_5.csv','rand95.csv');
 
 # Rename to standardized column names
 chirename <- rbind(c('ODDS_RATIO','OR')
                    );
 # Display names
-renameforplots <- rbind( c('REF','All Urology')
-                         ,c('M6921.I6921.R10661','Hispanic')
-                         ,c('M6922.I6922.R10663','Low Income')
-                   );
+renameforplots <- rbind( c('REF','Overall Population')
+                         ,c('M7002.I7002.R10840','Random Subcohort 85')
+                         ,c('M7001.I7001.R10838','Random Subcohort 95')
+                         ,c('M7003.I7003.R10842','Random Subcohort 89_5')
+                         );
 c()
--- infiles/demo/project_uitext.R
+++ infiles/demo/project_uitext.R
@@ -1,4 +1,4 @@
-txtMainVarStatic <- "The left-most columns represent all the patients ever seen by the Urology department. The other columns are cohorts within that patient pool as described by the plot legend."
-txtMainVarDynamic <- "The y-axis represents the fraction of all the patients ever seen by the Urology department who also have at least one occurence of a particular code. The x-axis represents the same thing but a fraction of the corresponding cohort WITHIN this patient pool as described by the plot legend. Points above/left of the diagonal line represent codes that are less common in their respective cohorts than in the overall Urology patient pool, and points below/right are more common."
-txtPageSubtitle <- "Urology early adopter alpha edition"
+txtMainVarStatic <- "The left-most columns represent a large simulated cohort and the others are random sub-cohorts sampled from it."
+txtMainVarDynamic <- "The y-axis represents the fraction of a large simulated cohort having at least one occurence of the indicated code. The x-axis represents the same but one of the random sub-cohorts as described by the plot legend. Points above/left of the diagonal line represent codes that are less common in their respective sub-cohorts than in the overall cohort, and points below/right are more common."
+txtPageSubtitle <- "Demo"
 selBasicDefault <- 'UTHSCSA|FINCLASS'
