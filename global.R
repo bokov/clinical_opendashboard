7ffdeff00624946e89eb5a432934db030dcc30b9
Alex Bokov <bokov@uthscsa.edu>
2020-02-03 13:57:27 -0600

    global.R; planning; Sketch for dynamic generation of demogcodes table

--- global.R
+++ global.R
@@ -60,6 +60,13 @@ codehr_init(file.path(infiles,'demo'),confenv = .GlobalEnv);
 # demogcodes should include the following columns: 'PREFIX','Category','CCD'
 # PREFIX and CCD are the same as used by ChinoType. Category is a human-readable
 # version of PREFIX.
+
+# Variables for columns
+# Example for implementing auto-generation of demogcodes from the data
+# subset(foo,PREFIX %in% c('UTHSCSA|FINCLASS','TOBACCO_USER','MYC_STATUS','MYC_RECV_EMAIL','MYC_ACCESSED','Institution','DischStat','DischDisp','DischargeStatus','DEM|VITAL','DEM|STATE','DEM|SEX','DEM|RELIGION','DEM|RACE','DEM|MARITAL','DEM|LANGUAGE','DEM|ETHNICITY'))[,c('PREFIX','CATEGORY','CCD')] %>% View
+# Variables for spots
+# subset(foo,!PREFIX %in% c('UTHSCSA|FINCLASS','TOBACCO_USER','MYC_STATUS','MYC_RECV_EMAIL','MYC_ACCESSED','Institution','DischStat','DischDisp','DischargeStatus','DEM|VITAL','DEM|STATE','DEM|SEX','DEM|RELIGION','DEM|RACE','DEM|MARITAL','DEM|LANGUAGE','DEM|ETHNICITY','GEO'))[,c('PREFIX','CATEGORY')] %>% unique %>% View
+# Need to update CODEHR_INIT to try to derive CSVs first
 selBasicChoices <- with(unique(demogcodes[,c('PREFIX','Category')])
                         ,c(grep('TOTAL',inv=T,val=T,setNames(PREFIX,Category))
                            ,c(`All Codes`='ALL')));
